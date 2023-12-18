997ed2d43a50e427ff07aeaab40e682900d427bc
Lucas Costa Valenca <val.computerscience@gmail.com>
2023-12-18 07:43:04 -0300

    new commit

--- 116/Main.py
+++ 116/Main.py
@@ -9,14 +9,14 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
-        if root is None:
+        if root == None:
             return None
         node1 = root.left
         node2 = root.right
-        while node1 is not None and node2 is not None:
+        while node1 != None:
             node1.next = node2
-            node1 = node1.right if node1.right is not None else node1.left
-            node2 = node2.left if node2.left is not None else node2.right
+            node1 = node1.right
+            node2 = node2.left
         self.connect(root.left)
         self.connect(root.right)
         return root
@@ -30,11 +30,7 @@ def create_tree(levels: int, value=0) -> Node:
     root.right = create_tree(levels-1, value*2+2)
     return root
 
-tree = Node(1)
-tree.left = Node(2)
-tree.right = Node(3)
-tree.left.left = Node(4)
-tree.right.right = Node(5)
+tree = create_tree(3)
 sol = Solution()
 connected_tree = sol.connect(tree)
 print("done")
